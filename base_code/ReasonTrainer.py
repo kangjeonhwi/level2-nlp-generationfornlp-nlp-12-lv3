c7eeb8abd465b1aca01a18d4db45efc40a0e7627
rasauq1122 <rasauq1122@gmail.com>
2024-11-18 15:13:52 +0900

    feat: add main routine

--- base_code/ReasonTrainer.py
+++ base_code/ReasonTrainer.py
@@ -132,3 +132,17 @@ class ReasonTrainer(MyTrainer):
         print("-" * 30)
         
         return trainer
+    
+
+if __name__ == "__main__":
+    import argparse
+    from utils.load import load_config
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="./config/config", help="path where config json is store")
+    args = parser.parse_args()
+    
+    config = load_config(args.config)
+    settings = config["settings"]
+    trainer = ReasonTrainer(settings["dataset"], settings["model_name"], config["params"])
+    trainer.train()
