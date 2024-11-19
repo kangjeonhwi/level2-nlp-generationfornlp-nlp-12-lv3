a4c5bd772d44fe122a96fbe65029981e5dea85a9
rasauq1122 <rasauq1122@gmail.com>
2024-11-19 18:37:10 +0900

    feat: add preprocess_logits_for_metrics() for memory optimization

--- base_code/ReasonTrainer.py
+++ base_code/ReasonTrainer.py
@@ -4,6 +4,7 @@ from trainer import MyTrainer
 from config.prompts import PROMPT_GEN_REASON_NO_QUESTION_PLUS, PROMPT_GEN_REASON_QUESTION_PLUS
 
 import pandas as pd
+import numpy as np
 from transformers import Trainer
 from datasets import Dataset
 from evaluate import load
@@ -56,21 +57,37 @@ class ReasonTrainer(MyTrainer):
             
         return Dataset.from_pandas(pd.DataFrame(processed_dataset))
 
+    # 모델의 logits 를 조정하여 정답 토큰 부분만 출력하도록 설정
+    def preprocess_logits_for_metrics(logits, labels):
+        # 로짓에서 가장 높은 확률의 토큰 인덱스를 추출 (argmax)
+        predictions = logits.argmax(dim=-1)
+        # print(predictions)
+        return predictions.cpu(), labels.cpu()
+
     def compute_metrics(self, eval_preds):
         metric = load("bleu")  # BLEU 평가 지표
-        logits, labels = eval_preds
-        predictions = logits.argmax(dim=-1)
-
-        # Flattening predictions and labels, ignoring padding tokens
-        predictions = predictions.view(-1).tolist()
-        labels = labels.view(-1).tolist()
-
-        # SacreBLEU expects a list of sentences
-        decoded_preds = [self.tokenizer.decode(p, skip_special_tokens=True) for p in predictions]
-        decoded_labels = [[self.tokenizer.decode(l, skip_special_tokens=True)] for l in labels]
-
-        return metric.compute(predictions=decoded_preds, references=decoded_labels)
-    
+        predictions, labels = eval_preds
+        predictions = predictions[0] # predictions = Tuple[np.ndarray]
+        
+        def find_non_pad(row):
+            return np.argmax(row != -100)
+        answer_indices = np.apply_along_axis(find_non_pad, axis=-1, arr=labels)
+        
+        for i, idx in enumerate(answer_indices):
+            predictions[i, :idx] = -100
+        labels = np.where(labels == -100, self.tokenizer.pad_token_id, labels)
+        predictions = np.where(predictions == -100, self.tokenizer.pad_token_id, predictions)
+  
+        decoded_preds = self.tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        decoded_labels = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
+  
+        result = metric.compute(
+            predictions=[pred for pred in decoded_preds],  # 토큰화된 예측값
+            references=[label for label in decoded_labels]  # 토큰화된 참조값
+        )
+  
+        return result
+  
     def load_dataset(self) -> Dataset:
         df = pd.read_csv(self.data_path + "/train.csv")
         records = []
@@ -89,70 +106,6 @@ class ReasonTrainer(MyTrainer):
                 record['question_plus'] = problems['question_plus']
             records.append(record)
         return Dataset.from_pandas(pd.DataFrame(records))
-
-    def get_trainer(self) -> Trainer:
-        """Trainer 객체를 생성합니다. 다음 인스턴스 변수들이 존재해야 합니다.
-        - model: 훈련시킬 모델
-        - train_dataset: 훈련 데이터셋
-        - eval_dataset: 검증 데이터셋
-        - data_collator: 데이터 콜레이터 함수
-        - tokenizer: 토크나이저
-        - params: 훈련에 필요한 하이퍼파라미터
-
-        Returns:
-            Trainer: Trainer 객체를 반환합니다.
-        """
-        peft_config = LoraConfig(
-            r=6,
-            lora_alpha=8,
-            lora_dropout=0.05,
-            target_modules=['q_proj', 'k_proj'],
-            bias="none",
-            task_type="CAUSAL_LM",
-        )
-
-        sft_config = SFTConfig(
-            do_train=True,
-            do_eval=False,
-            lr_scheduler_type=self.params["lr_scheduler_type"],
-            max_seq_length=self.params["max_seq_length"],
-            output_dir=self.params["output_dir"],
-            per_device_train_batch_size=self.params["train_batch_size"],
-            per_device_eval_batch_size=self.params["eval_batch_size"],
-            num_train_epochs=self.params["epoch"],
-            learning_rate=float(self.params["learning_rate"]),
-            weight_decay=self.params["weight_decay"],
-            logging_steps=100,
-            save_strategy="epoch",
-            eval_strategy="no",
-            save_total_limit=2,
-            save_only_model=True,
-            report_to="none",
-            log_level='error'
-        )
-
-        trainer = SFTTrainer(
-            model=self.model,
-            train_dataset=self.train_dataset,
-            eval_dataset=self.eval_dataset,
-            data_collator=self.data_collator,
-            tokenizer=self.tokenizer,
-            compute_metrics=self.compute_metrics,
-            peft_config=peft_config,
-            args=sft_config
-        )
-        
-        print("-" * 30)
-        print("lr_scheduler_type : {}".format(self.params["lr_scheduler_type"]))
-        print("max_seq_length : {}".format(self.params["max_seq_length"]))
-        print("train_batch_size : {}".format(self.params["train_batch_size"]))
-        print("eval_batch_size : {}".format(self.params["eval_batch_size"]))
-        print("epoch : {}".format(self.params["epoch"]))
-        print("learning_rate : {}".format(self.params["learning_rate"]))
-        print("weight_decay : {}".format(self.params["weight_decay"]))
-        print("-" * 30)
-        
-        return trainer
     
 
 if __name__ == "__main__":
