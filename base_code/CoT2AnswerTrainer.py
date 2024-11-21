d71b153266e2a8fc78c69db690c398a6642b8338
rasauq1122 <rasauq1122@gmail.com>
2024-11-21 11:40:01 +0900

    feat: disable dynamic choice length prompt

--- base_code/CoT2AnswerTrainer.py
+++ base_code/CoT2AnswerTrainer.py
@@ -48,8 +48,8 @@ class CoT2AnswerTrainer(MyTrainer):
                     reason=dataset[i]["reason"],
                 )
 
-            if len(dataset[i]["choices"]) == 4:
-                user_message = user_message.replace("1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.", "1, 2, 3, 4 중에 하나를 정답으로 고르세요.")
+            # if len(dataset[i]["choices"]) == 4:
+            #     user_message = user_message.replace("1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.", "1, 2, 3, 4 중에 하나를 정답으로 고르세요.")
 
             # chat message 형식으로 변환
             processed_dataset.append(
