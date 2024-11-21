1ac3fb8d51c4ac5e70d0fd288038dce86c7b1e87
rasauq1122 <rasauq1122@gmail.com>
2024-11-21 11:36:16 +0900

    feat: add main routine

--- base_code/CoT2AnswerTrainer.py
+++ base_code/CoT2AnswerTrainer.py
@@ -44,3 +44,16 @@ class CoT2AnswerTrainer(MyTrainer):
             )
         
         return Dataset.from_pandas(pd.DataFrame(processed_dataset))
+
+if __name__ == "__main__":
+    import argparse
+    from utils.load import load_config
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="./config/config", help="path where config json is store")
+    args = parser.parse_args()
+    
+    config = load_config(args.config)
+    settings = config["settings"]
+    trainer = CoT2AnswerTrainer(settings["dataset"], settings["model_name"], config["params"])
+    trainer.train()
