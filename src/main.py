9969efcc48e6190ce892e0ae16cbadf30545de5d
rasauq1122 <rasauq1122@gmail.com>
2024-11-22 21:19:54 +0900

    feat: add argument for do train or inference

--- src/main.py
+++ src/main.py
@@ -1,11 +1,34 @@
 import argparse
+import json
 from pipeline import BasePipeline
 from manager import BaselineManager
+
+def get_model_name(config):
+    with open(config + ".json", "r") as f:
+        config = json.load(f)
+    return config["model"]["name"]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default="./config/baseline", help="path where config json is store")
+    parser.add_argument("--train", action="store_true", help="train the model")
+    parser.add_argument("--inference", action="store_true", help="inference the model")
+    parser.add_argument("--do_both", action="store_true", help="train and inference the model")
     args = parser.parse_args()
 
+    if not args.train and not args.inference and not args.do_both:
+        print("No action specified. Do train.")
+        args.train = True 
+    
+    if args.do_both:
+        args.train = True
+        args.inference = True
+    
     pipeline = BasePipeline(args.config, BaselineManager)
-    pipeline.train()
+    
+    if args.train:
+        pipeline.train()
+    if args.inference:
+        if not args.train and not "checkpoint" in get_model_name(args.config):
+            print("! ! ! No checkpoint found. Vanilla model will be used for inference. ! ! !")
+        pipeline.inference()
