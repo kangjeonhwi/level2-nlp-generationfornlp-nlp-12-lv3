43b34b5d8d0cb563fc96240134d78305f4cf48db
rasauq1122 <rasauq1122@gmail.com>
2024-11-22 13:21:25 +0900

    feat: add GemmaManager.py

---
+++ base_code/GemmaManager.py
@@ -0,0 +1,14 @@
+from typing import Type, Union
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoModelForCausalLM
+from ModelManager import ModelManager
+
+class GemmaManager(ModelManager):
+    
+    def set_model(self, AutoModel: Union[Type[AutoModelForCausalLM], Type[AutoPeftModelForCausalLM]] = AutoModelForCausalLM):
+        self.model = AutoModel.from_pretrained(
+            self.model_name_or_checkpoint,
+            trust_remote_code=True,
+            device_map="auto"
+        )
+    
