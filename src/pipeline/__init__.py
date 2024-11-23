3ab76d01548546ce8564d62cdfce014f19a9bb04
rasauq1122 <rasauq1122@gmail.com>
2024-11-23 15:22:18 +0900

    feat: add vllm pipeline (only for generation)

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,9 +1,11 @@
 from .BasePipeline import BasePipeline
 from .BaseLogitPipeline import BaseLogitPipeline
 from .GenPipeline import GenPipeline
+from .VLLMPipeline import VLLMPipeline
 
 __all__ = [
     "BasePipeline",
     "BaseLogitPipeline",
     "GenPipeline",
+    "VLLMPipeline",
 ]
